1a5ad6c4a74426439f0d71706b5ac7c7fa434f55
pilyct <45486109+pilyct@users.noreply.github.com>
2026-04-21 23:13:43 +0200

    fix: SonarQube issues

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,17 @@
-
-import express from 'express';
-import cors from 'cors';
-import router from './router';
+import express from "express";
+import cors from "cors";
+import router from "./router";
 
 const app = express();
+app.disable("x-powered-by");
+
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173/",
+  }),
+);
 app.use(express.json());
 app.use(router);
 
