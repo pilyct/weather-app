a19de6b729eb0031858583153b6cf4f0d16deb5c
pilyct <45486109+pilyct@users.noreply.github.com>
2026-04-21 19:32:02 +0200

    fix: weather geo url

--- server/models/weatherData.ts
+++ server/models/weatherData.ts
@@ -1,18 +1,18 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 config();
 
 interface OpenWeatherMapConfig {
   BASE_URL: string;
   GEO_URL: string;
+  GEO_REVERSE_URL: string;
   API_KEY: string | undefined;
 }
 
 const openWeatherMap: OpenWeatherMapConfig = {
-  BASE_URL: 'https://api.openweathermap.org/data/2.5',
-  GEO_URL: 'http://api.openweathermap.org/geo/1.0/direct',
-  API_KEY: process.env.WEATHER_SECRET_KEY
+  BASE_URL: "https://api.openweathermap.org/data/2.5",
+  GEO_URL: "https://api.openweathermap.org/geo/1.0/direct",
+  GEO_REVERSE_URL: "https://api.openweathermap.org/geo/1.0/reverse",
+  API_KEY: process.env.WEATHER_SECRET_KEY,
 };
 
 export default openWeatherMap;
-
-
